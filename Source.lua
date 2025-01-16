b3a918d17256930f2cabe71258274d191e4ebac6
gabrelso <146791410+gabrelso@users.noreply.github.com>
2025-01-16 04:44:32 -0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -10,17 +10,17 @@ local PlayerMouse = Player:GetMouse()
 
 local redzlib = {
 	Themes = {
-		Darker = {
+		Purple = {
 			["Color Hub 1"] = ColorSequence.new({
-				ColorSequenceKeypoint.new(0.00, Color3.fromRGB(0, 0, 0)),
-				ColorSequenceKeypoint.new(0.50, Color3.fromRGB(30, 0, 0)),
-				ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 0, 0))
+				ColorSequenceKeypoint.new(0.00, Color3.fromRGB(10, 0, 0)),  
+				ColorSequenceKeypoint.new(0.50, Color3.fromRGB(25, 0, 0)),  
+				ColorSequenceKeypoint.new(1.00, Color3.fromRGB(10, 0, 0))   
 			}),
-			["Color Hub 2"] = Color3.fromRGB(0, 0, 0),
-			["Color Stroke"] = Color3.fromRGB(100, 0, 0),
-			["Color Theme"] = Color3.fromRGB(255, 0, 0),
-			["Color Text"] = Color3.fromRGB(240, 240, 240),
-			["Color Dark Text"] = Color3.fromRGB(180, 180, 180)
+			["Color Hub 2"] = Color3.fromRGB(30, 0, 0),  
+			["Color Stroke"] = Color3.fromRGB(40, 0, 0),  
+			["Color Theme"] = Color3.fromRGB(255, 0, 0),  
+			["Color Text"] = Color3.fromRGB(240, 240, 240),  
+			["Color Dark Text"] = Color3.fromRGB(180, 180, 180)  
 		}
 	},
 	Info = {
@@ -29,7 +29,7 @@ local redzlib = {
 	Save = {
 		UISize = {550, 380},
 		TabSize = 160,
-		Theme = "Darker"
+		Theme = "Purple"
 	},
 	Settings = {},
 	Connection = {},
@@ -40,6 +40,7 @@ local redzlib = {
 	Tabs = {},
 	Icons = loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/RedzLibV5/refs/heads/main/Icons.lua"))()
 }
+
 
 local ViewportSize = workspace.CurrentCamera.ViewportSize
 UIScale = ViewportSize.Y / 450
